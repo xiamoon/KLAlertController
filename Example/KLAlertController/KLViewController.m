743d81d2a2f770aaf028fce681d7a6a6d79b7cd3
李乾 <qian.li@gailvlun.cn>
2020-01-04 11:16:23 +0800

    添加测试代码

--- Example/KLAlertController/KLViewController.m
+++ Example/KLAlertController/KLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "KLViewController.h"
+#import "KLAlertController.h"
 
 @interface KLViewController ()
 
@@ -17,7 +18,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)testAlert:(id)sender {
+    KLAlertController *alert = [KLAlertController alertControllerWithTitle:@"你好" message:@"这是一条测试信息" preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addAction:[KLAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(KLAlertAction * _Nonnull action) {
+        
+    }]];
+    [alert addAction:[KLAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(KLAlertAction * _Nonnull action) {
+        
+    }]];
+    
+    [alert kl_show];
+}
+
+- (IBAction)testSheet:(id)sender {
+    KLAlertController *alert = [KLAlertController alertControllerWithTitle:@"你好" message:@"这是一条测试信息" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [alert addAction:[KLAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(KLAlertAction * _Nonnull action) {
+        
+    }]];
+    [alert addAction:[KLAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(KLAlertAction * _Nonnull action) {
+        
+    }]];
+    
+    [alert kl_show];
 }
 
 - (void)didReceiveMemoryWarning
